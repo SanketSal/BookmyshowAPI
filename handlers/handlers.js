017bc41a4a8ffdcf288840636735e9745e684c9b
SanketSal <40586810+SanketSal@users.noreply.github.com>
2023-08-20 22:49:44 +0530

    Update handlers.js

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -1,133 +1,72 @@
-const { Sequelize, Op } = require('sequelize');
-const { updateRedisCache, getDataFromCache } = require('./helpers');
-const { Theatre, Movie, Showtime, Seat } = require('./models');
+// Import the necessary models and modules
+const { Comment, Rating } = require('./models');
 
-// Handlers
-async function getDatesHandler(req, res) {
-  const { theatre_id } = req.params;
-  try {
-    const redisKey = { theatre_id };
-    const cachedDates = await getDataFromCache(redisKey);
-
-    if (cachedDates) {
-      console.log('Dates fetched from Redis cache');
-      res.json(cachedDates);
-    } else {
-      const dates = await Showtime.findAll({
-        attributes: [
-          [Sequelize.fn('DISTINCT', Sequelize.col('show_date')), 'show_date'],
-        ],
-        where: {
-          theatre_id: theatre_id,
-          show_date: { [Op.gte]: new Date() },
-        },
-        order: ['show_date'],
-        limit: 7,
-      });
-
-      await updateRedisCache(redisKey, dates);
-
-      console.log('Dates fetched from database and cached in Redis');
-      res.json(dates.map((date) => date.show_date));
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
-
-async function getMoviesHandler(req, res) {
-  const { theatre_id, show_date } = req.params;
-  try {
-    const redisKey = { theatre_id, show_date };
-    const cachedMovies = await getDataFromCache(redisKey);
-
-    if (cachedMovies) {
-      console.log('Movies fetched from Redis cache');
-      res.json(cachedMovies);
-    } else {
-      const movies = await Movie.findAll({
-        attributes: ['movie_id', 'title'],
-        include: [
-          {
-            model: Showtime,
-            attributes: ['show_time'],
-            where: {
-              theatre_id: theatre_id,
-              show_date: show_date,
-            },
-            include: [
-              {
-                model: Seat,
-                attributes: ['seat_number', 'is_booked'],
-              },
-            ],
-          },
-        ],
-      });
-
-      const formattedMovies = movies.map((movie) => ({
-        movie_id: movie.movie_id,
-        title: movie.title,
-        showtimes: movie.Showtimes.map((showtime) => ({
-          show_time: showtime.show_time,
-          available_seats: showtime.Seats.filter((seat) => !seat.is_booked).map(
-            (seat) => seat.seat_number
-          ),
-        })),
-      }));
-
-      await updateRedisCache(redisKey, formattedMovies);
-
-      console.log('Movies fetched from database and cached in Redis');
-      res.json(formattedMovies);
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
-
-async function bookSeatsHandler(req, res) {
-  const { theatre_id, showtime_id } = req.params;
-  const { seats } = req.body;
+// Add a comment for a specific movie
+async function addCommentHandler(req, res) {
+  const { movie_id } = req.params;
+  const { text } = req.body;
 
   try {
-    // Fetch the selected showtime and associated seats
-    const showtime = await Showtime.findByPk(showtime_id, {
-      include: [Seat],
+    const comment = new Comment({
+      movie_id,
+      text,
+      // You can add other attributes as needed
     });
 
-    // Check if any of the selected seats are already booked
-    const alreadyBookedSeats = showtime.Seats.filter((seat) =>
-      seats.includes(seat.seat_number) && seat.is_booked
-    );
-
-    if (alreadyBookedSeats.length > 0) {
-      return res.status(400).json({ error: 'Some seats are already booked' });
-    }
-
-    // Update seat booking status in the database and Redis cache
-    const bookedSeats = await Seat.update(
-      { is_booked: true },
-      {
-        where: {
-          showtime_id: showtime_id,
-          seat_number: { [Op.in]: seats },
-        },
-        returning: true,
-      }
-    );
-
-    // Update Redis cache with the latest seat booking status
-    await updateRedisCache(showtime_id, bookedSeats.map((seat) => seat.seat_number));
-
-    console.log('Seats booked successfully');
-    res.json({ message: 'Seats booked successfully' });
+    await comment.save();
+    console.log('Comment added successfully');
+    res.status(201).json(comment);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 }
 
-module.exports = { getDatesHandler, getMoviesHandler, bookSeatsHandler };
+// Retrieve comments for a specific movie
+async function getCommentsHandler(req, res) {
+  const { movie_id } = req.params;
+
+  try {
+    const comments = await Comment.find({ movie_id }).exec();
+    res.json(comments);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+// Add a rating for a specific movie
+async function addRatingHandler(req, res) {
+  const { movie_id } = req.params;
+  const { rating } = req.body;
+
+  try {
+    const newRating = new Rating({
+      movie_id,
+      rating,
+      // You can add other attributes as needed
+    });
+
+    await newRating.save();
+    console.log('Rating added successfully');
+    res.status(201).json(newRating);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+// Retrieve ratings for a specific movie
+async function getRatingsHandler(req, res) {
+  const { movie_id } = req.params;
+
+  try {
+    const ratings = await Rating.find({ movie_id }).exec();
+    res.json(ratings);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+module.exports = { addCommentHandler, getCommentsHandler, addRatingHandler, getRatingsHandler };
