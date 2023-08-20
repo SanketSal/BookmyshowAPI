f2f6c19feedbc2d5cbb3420dd6bf05ca02ee7d87
SanketSal <40586810+SanketSal@users.noreply.github.com>
2023-08-20 22:47:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON data in the request body
 app.use(express.json());
-
 app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
